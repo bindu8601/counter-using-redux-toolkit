23aa931be6544acf2bd53b7aba02f3ba6c4ff46c
bindu8601 <bindueppalapalle@gmail.com>
2023-04-05 11:38:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { increment,decrement,incrementByAmount } from './redux/counterSlice';
+import { increment,decrement,incrementByAmount, decrementByAmount } from './redux/counterSlice';
 import {useSelector, useDispatch} from 'react-redux';
 import { useState } from 'react';
 import './style.css'
 
 function App() {
-  const [incrementAmount, setIncrementAmount] = useState(1);
+  const [Amount, setAmount] = useState(1);
   const count = useSelector((state) => state.mycount.value)
   const dispatch = useDispatch()
   return (
@@ -14,8 +14,9 @@ function App() {
       <h1>{count}</h1>
       <button onClick={()=>dispatch(increment())}>Increment by 1</button>
       <button onClick={()=>dispatch(decrement())}> Decrement by 1</button>
-      <input type='number' value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)}/>
-      <button onClick={()=>dispatch(incrementByAmount(Number(incrementAmount)))}>Increment by amount</button>
+      <input type='number' value={Amount} onChange={(e) => setAmount(e.target.value)}/>
+      <button onClick={()=>dispatch(incrementByAmount(Number(Amount)))}>Increment by amount</button>
+      <button onClick={()=>dispatch(decrementByAmount(Number(Amount)))}>Decrement by amount</button>
     </div>
   )
 }
